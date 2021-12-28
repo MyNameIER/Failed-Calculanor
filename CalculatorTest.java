ac13ab99a40d88bfba7934fcbac050b24bf7e624
Reshetnikov Ivan <mash-74@yandex.ru>
2021-12-28 16:17:34 +0300

    My failed attempt. Version1.0

---
+++ CalculatorTest.java
@@ -0,0 +1,33 @@
+package Test4;
+
+import java.util.Scanner;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        int sum = 0;
+        int k = 0;
+        Scanner ad = new Scanner(System.in);
+        int n = ad.nextInt();
+        int m = ad.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            int Cn = ad.nextInt();
+            array[i] = Cn;
+            sum = array[i] + sum;
+        }
+        int result = sum / m;
+        if (result < 1) {
+            System.out.println("0");
+            return;
+        }
+        result++;
+        while (k < m) {
+            k = 0;
+            for (int i = 0; i < n; i++) {
+                k = array[i] / (result - 1) + k;
+            }
+            result--;
+        }
+        System.out.println(result);
+    }
+}
